5fb87157b5d4ad7d6b4cf84c9745d27e5fc6c966
arunthegiri <arunthegiri@gmail.com>
2026-05-29 02:53:35 -0700

    feat: add HomeScreen stub

---
+++ src/main/java/com/kbbq999/screens/HomeScreen.java
@@ -0,0 +1,16 @@
+package com.kbbq999.screens;
+
+import java.util.Scanner;
+
+public class HomeScreen {
+
+    private Scanner scanner;
+
+    public HomeScreen(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void display() {}
+    public int getUserChoice() { return 0; }
+    public void handleInput(int choice) {}
+}
